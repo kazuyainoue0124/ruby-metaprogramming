3428ff1a86b30c7e63a0170932e6ed941152d1fc
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-29 08:45:00 +0900

    to_alphanumericメソッドとテストの実装

---
+++ object_model/alphanumeric.rb
@@ -0,0 +1,3 @@
+def to_alphameric(s)
+  s.gsub(/[^a-zA-Z0-9]/, '')
+end
---
+++ object_model/to_alphanumeric_test.rb
@@ -0,0 +1,7 @@
+require 'test/unit'
+
+class ToAlphanumericTest < Test::Unit::TestCase
+  def test_strip_non_alphanumeric_characters
+    assert_equal '3 the Magic Number', to_alphanumeric('#3, the *Magic, Number*?')
+  end
+end

d3fbf251c8fe67a16be7d5f6db4f269be3a44f58
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-30 08:36:06 +0900

    prependメソッドでモジュールを継承チェーンに加える例

--- object_model/modules_include.rb
+++ object_model/modules_include.rb
@@ -22,3 +22,28 @@ puts D.ancestors
 # Kernel
 # BasicObject
 # と表示される
+
+
+## prependメソッドを使って継承チェーンにモジュールを追加することもできる
+module M2
+  def my_method
+    'M2#my_method()'
+  end
+end
+
+# prependメソッドを使ってM2モジュールを追加する
+# 継承チェーンはC2クラスの１つ下にM2モジュールが追加される
+class C2
+  prepend M2
+end
+
+class D2 < C2; end
+
+puts D2.ancestors
+# D2
+# M2
+# C2
+# Object
+# Kernel
+# BasicObject
+# と表示される

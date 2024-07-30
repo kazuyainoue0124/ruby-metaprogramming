a799551492e2b064f85e779298949ec0390b1402
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-31 08:54:31 +0900

    sendメソッドを用いた動的なメソッド呼び出し

---
+++ methods/dynamic_call.rb
@@ -0,0 +1,15 @@
+class MyClass
+  def my_method(my_arg)
+    my_arg * 2
+  end
+end
+
+obj = MyClass.new
+# ドット記法を用いた通常のメソッド呼び出し
+puts obj.my_method(3) # => 6
+
+# sendメソッドを用いたメソッド呼び出し
+# 第1引数にはメソッド名をシンボルで指定（文字列でも可だがシンボルはイミュータブルなので最適）
+# 第2引数以降にはメソッドに渡す引数を指定
+# sendメソッドを使えばコードの実行時にメソッドを動的に呼び出すことができる
+puts obj.send(:my_method, 3) # => 6

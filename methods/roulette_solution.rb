39425f1d29c3f5a4337c62dc82f47e378820b2fa
Kazuya INOUE <bauer24tbp@gmail.com>
2024-08-02 09:07:36 +0900

    Rouletteの改良版

---
+++ methods/roulette_solution.rb
@@ -0,0 +1,24 @@
+class Roulette
+  def method_missing(name, *args)
+    person = name.to_s.capitalize
+    super unless %w[Bob Frank Bill].include? person
+    number = 0
+    3.times do
+      number = rand(10) + 1
+      puts "#{number}..."
+    end
+    "#{person} got a #{number}"
+  end
+end
+
+number_of = Roulette.new
+puts number_of.bob
+puts number_of.frank
+# 5...
+# 6...
+# 7...
+# Bob got a 7
+# 8...
+# 2...
+# 5...
+# Frank got a 5

09f5321082464c45791ef4ce3374a4548822d9e7
Kazuya INOUE <bauer24tbp@gmail.com>
2024-08-15 08:39:18 +0900

    特異メソッドにより不要なクラスを削除できる例

--- class_definitions/paragraph.rb
+++ class_definitions/paragraph.rb
@@ -10,3 +10,17 @@ class Paragraph
   def upcase; @text.upcase; end
   # ...
 end
+
+# Paragraph#title?　はアプリケーション全体で１回だけ呼び出されている
+def index(paragraph)
+  add_to_index(paragraph) if paragraph.title?
+end
+
+# どんな文字列も段落になる
+paragraph = "any string can be a paragraph"
+
+def paragraph.title?
+  self.upcase == self
+end
+
+index(paragraph)

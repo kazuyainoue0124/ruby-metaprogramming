7a2be1fdf25ab15de22ebddbeec34b19e47a8008
Kazuya INOUE <bauer24tbp@gmail.com>
2024-08-20 09:12:45 +0900

    ブロックを使ってバリデーションを実装する例

--- ctwc/checked_attributes/no_eval.rb
+++ ctwc/checked_attributes/no_eval.rb
@@ -1,3 +1,31 @@
+require 'test/unit'
+
+class Person; end
+
+class TestCheckedAttributes < Test::Unit::TestCase
+  def setup
+    add_checked_attribute(Person, :age)
+    @bob = Person.new
+  end
+
+  def test_accepts_valid_values
+    @bob.age = 20
+    assert_equal 20, @bob.age
+  end
+
+  def test_refuses_nil_values
+    assert_raises RuntimeError, 'Invalid attribute' do
+      @bob.age = nil
+    end
+  end
+
+  def test_refuses_false_values
+    assert_raises RuntimeError, 'Invalid attribute' do
+      @bob.age = false
+    end
+  end
+end
+
 # eval を使うデメリット
 # - デバッグが難しい
 # - パフォーマンスが悪い
